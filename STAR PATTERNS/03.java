aa665372430b5ad723a979ee95bb7ebadf7a556e
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-15 12:57:51 +0530

    Create 03.java

---
+++ STAR PATTERNS/03.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    int n= sc.nextInt();
+	    
+	    for(int i=0;i<n;i++){
+	        for(int k=0 ;k<i;k++){
+	            
+	            System.out.print(" ");
+	            
+	        }
+	        for(int j=0;j<n;j++){
+	            System.out.print("*");
+	            }
+	        
+	        
+	        System.out.println();
+	    }
+	    
+	}
+}
