c7f152bf3b753e01ab8322ce4289ba701973136d
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-16 19:20:35 +0530

    Create 06.java

---
+++ NUMBER PATTERNS/06.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count=10;
+    
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                
+                    System.out.printf("%-2d ",count--);
+            
+        }
+        System.out.println();
+        
+        }
+        
+    }
+}
+
+
+4
+10 9  8  7  
+6  5  4  
+3  2  
+1 
