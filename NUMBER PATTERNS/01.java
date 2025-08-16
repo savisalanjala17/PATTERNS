8ad0235309ff3fde4f26a00d0b44494ef1c58e21
savisalanjala17 <savisalanjala17@gmail.com>
2025-08-16 18:54:58 +0530

    Create 01.java

---
+++ NUMBER PATTERNS/01.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                System.out.print("1");
+            
+        }
+        System.out.println();
+        
+        }
+        
+    }
+}
+
+
+OUTPUT:
+
+4
+1111
+1111
+1111
+1111
